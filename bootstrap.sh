5d4e1b1a67502a9fa3828a874d499e5ba82ba093
fesari <30542611+fesari@users.noreply.github.com>
2025-05-12 15:25:34 +1000

    fixed incorrectly named files and added bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,25 @@
 
 DOTFILES="$HOME/.dotfiles"
 
-ln -sf "$DOTFILES/.zshrc" ~/.zshrc
-ln -sf "$DOTFILES/.zprofile" ~/.zprofile
-ln -sf "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
-ln -sf "$DOTFILES/.gitconfig" ~/.gitconfig
-ln -sf "$DOTFILES/.tmux.conf" ~/.tmux.conf
-ln -sf "$DOTFILES/.alacritty.toml" ~/.alacritty.toml
-ln -sf "$DOTFILES/.profile" ~/.profile
+link() {
+  local file="$1"
+  local source="$DOTFILES/$file"
+  local dest="$HOME/$file"
+
+  if [ -e "$dest" ]; then
+    echo "[>] Skipping $file — already exists in ~"
+  elif [ -f "$source" ]; then
+    ln -s "$source" "$dest"
+    echo "[§] Linked $dest → $source"
+  else
+    echo "[!] Skipping $file — source file does not exist in .dotfiles"
+  fi
+}
+
+# List all files you want symlinked
+link .zshrc
+link .zprofile
+link .p10k.zsh
+link .gitconfig
+link .tmux.conf
+link .alacritty.toml
